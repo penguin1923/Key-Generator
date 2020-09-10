fd99a48850cac42838e9a031f6218257a5fb12fc
Don <64542224+penguin1923@users.noreply.github.com>
2020-09-10 10:57:07 -0400

    rearagnged the menu structure still need to continue to make breakproof

--- CD Key Generator/Program.cs
+++ CD Key Generator/Program.cs
@@ -9,51 +9,10 @@ namespace CD_Key_Generator
     {
         static void Main(string[] args)
         {
-            Generator newKey = new Generator();
-            while (true)
-            {
-                Console.WriteLine("Welcome To The Program Key Generator");
-                Console.WriteLine("1:Generate a new Program Key");
-                Console.WriteLine("2:Decrypt a Program Key");
-                Console.WriteLine("Q:Quit");
-                Console.WriteLine("");
-
-                string input = Console.ReadLine();
-
-                if (input == "1")
-                {
-
-                    //Ask for length and character types from users
-                    Console.WriteLine("What is the length of the Program Key?");
-                    int keyLength = int.Parse(Console.ReadLine());
-                    //int.parse is to convert the string to a integer
-                    Console.WriteLine("what Types of Characters are you going use?");
-                    Console.WriteLine("1:Characters only A-Z");
-                    Console.WriteLine("2:Numbers only 0-9");
-                    Console.WriteLine("3:Characters and Numbers");
-
-                    string option=Console.ReadLine();
-                    //This is happy user and can definitly be broken no validation yet
-                    Console.WriteLine("So you Program Key will be "+keyLength+" characters long and use option "+option);
-                    Console.WriteLine("Is this correct? Y or N");
-                    string conformation=Console.ReadLine();
-
-                    if (conformation == "Y" || conformation == "y")
-                    {//run program
-                        newKey.ProgramKey(option,keyLength);
-                    }
-                    else
-                    {//return to input==1 or break to the beginning
-                        
-                    }
-                }
-                else if (input == "Q" || input == "q")
-                {
-                    break;
-                }
-                Console.ReadLine();
-                Console.Clear();
-            }
+            
+            Menu menu = new Menu();
+            menu.MainMenu();
         }
+
     }
 }
