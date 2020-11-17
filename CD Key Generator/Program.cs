d79285ea4ec4ff31f26507cf074e2e7c51139c89
Don <64542224+penguin1923@users.noreply.github.com>
2020-11-16 20:31:19 -0500

    added prometheus call

--- CD Key Generator/Program.cs
+++ CD Key Generator/Program.cs
@@ -2,15 +2,22 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using Prometheus;
 
 namespace CD_Key_Generator
 {
     public class Program
     {
+        private static readonly Counter TickTock =
+       Metrics.CreateCounter("Cdup", "Just keeps on ticking");
+
         static void Main(string[] args)
         {
             
             Menu menu = new Menu();
+            var server = new MetricServer(hostname: "localhost", port: 1234);
+            server.Start();
+            TickTock.Inc();
             menu.MainMenu();
         }
 
